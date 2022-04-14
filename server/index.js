9a1cbe0aa12a4b5d15c39f5dda27c4ade1daa4e9
sylviajebet <sylviajebett@gmail.com>
2022-04-15 02:01:43 +0300

    node modules replaced

--- server/index.js
+++ server/index.js
@@ -2,10 +2,33 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+//const pg = require('pg');
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+//create one work
+
+app.post("/work", (req, res) => {
+    try {
+
+        console.log(req.body);
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get all work
+
+//get one work
+
+//update one work
+
+//delete one work
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
