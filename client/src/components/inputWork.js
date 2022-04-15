ee47e214fa80e9333e91cdcc62274f5dd3658176
sylviajebet <sylviajebett@gmail.com>
2022-04-15 04:12:56 +0300

    Form updated

--- client/src/components/inputWork.js
+++ client/src/components/inputWork.js
@@ -1,11 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputWork = () => {
+
+    const [subject, setSubject] = useState("")
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Homework List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control"/>
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    value={subject}
+                    onChange={e => setSubject(e.target.value)}
+                />
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
