48558a408d4393641618e8b2e765e9f9d88c4b14
sylviajebet <sylviajebett@gmail.com>
2022-04-15 04:05:38 +0300

    Form input field added

--- client/src/components/inputWork.js
+++ client/src/components/inputWork.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const InputWork = () => {
-    return <h1>Input Homework</h1>;
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5">Homework List</h1>
+            <form className="d-flex mt-5">
+                <input type="text" className="form-control"/>
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+    );
 };
 
 export default InputWork;
