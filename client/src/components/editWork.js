d6185dcc6ef0d13dd4438ccc3da26f28283847a8
sylviajebet <sylviajebett@gmail.com>
2022-04-15 13:10:09 +0300

    Edit button updated

--- client/src/components/editWork.js
+++ client/src/components/editWork.js
@@ -3,6 +3,23 @@ import React, { Fragment, useState } from "react";
 const EditWork = ({ workk }) => {
     const [subject, setSubject] = useState(workk.subject);
 
+    //edit subject function
+    const updateWork = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { subject };
+            const response = await fetch(`http://localhost:5000/work/${workk.work_id}`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+
+                window.location = "/";
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     return (
         <Fragment>
         <button type="button" 
@@ -17,22 +34,28 @@ const EditWork = ({ workk }) => {
         {/* 
             id = id10
         */}
-        <div class="modal" id={`id${workk.work_id}`}>
+        <div class="modal" id={`id${workk.work_id}`} onClick={() => setSubject(workk.subject)}>
         <div class="modal-dialog">
             <div class="modal-content">
 
             <div class="modal-header">
                 <h4 class="modal-title">Edit Work</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close" data-dismiss="modal" onClick={() => setSubject(workk.subject)}>&times;</button>
             </div>
 
             <div class="modal-body">
-                <input type="text" className="form-control" value={subject} />
+                <input type="text" className="form-control" value={subject} onChange={e => setSubject(e.target.value)}/>
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                <button type="button" 
+                        class="btn btn-warning" data-dismiss="modal"
+                        onClick = {e => updateWork(e)}
+                        >
+                    Edit
+                </button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal" onClick={() => setSubject(workk.subject)}>Close</button>
             </div>
 
             </div>
